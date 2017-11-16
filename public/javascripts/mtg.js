ccd130c5c19c90205915bb83c8e3d0df79ae2a3c
Fletcher243 <mumonrift@gmail.com>
2017-11-16 07:52:36 +0000

    working model

---
+++ public/javascripts/mtg.js
@@ -0,0 +1,48 @@
+angular.module('cardeck', [])
+.controller('MainCtrl', [
+  '$scope','$http',
+  function($scope,$http){
+
+  $scope.mycards = [];
+  $scope.deckcards = [];
+  
+  $scope.cardField = '';
+  $scope.displayText = '';
+
+  $scope.addCard = function(card){
+   return $http.post('/addcard', card).success(function(data){
+      console.log("card added");
+    });
+  }
+
+  $scope.removeCard = function(card){
+    $http.delete('/removecard/' + card._id)
+    .success(function(data){
+      console.log("card removed!");
+    });
+   $scope.getDeck();
+  }
+
+  $scope.getCards = function() {
+    if($scope.cardField == "") { return;}
+    $scope.displayText = "Click on a card to add it to the deck!"
+    var myurl= "/getcard?q=";
+    myurl += $scope.cardField;
+    $scope.deckcards = [];
+    return $http.get(myurl).success(function(data){
+
+      angular.copy(data, $scope.mycards);
+
+    });
+}
+
+  $scope.getDeck = function() {
+    $scope.displayText = "This is the current deck"
+      $scope.mycards = [];
+    return $http.get('getdeck').success(function(data){
+     
+      angular.copy(data, $scope.deckcards);
+
+    });
+}
+}]);
--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,11 @@ var mongoose = require('mongoose'); //Adds mongoose as a usable dependency
 mongoose.connect('mongodb://localhost/deckDB', { useMongoClient: true }); //Connects to a mongo database called "commentDB"
 
 var commentSchema = mongoose.Schema({ //Defines the Schema for this database
-Name: String,
-Image: String,
-Color: String,
-Type: String
+name: String,
+imageUrl: String
 });
 
-var Comment = mongoose.model('Comment', commentSchema); //Makes an object from that schema as a model
+var Deck = mongoose.model('Deck', commentSchema); //Makes an object from that schema as a model
 
 var db = mongoose.connection; //Saves the connection as a variable to use
 db.on('error', console.error.bind(console, 'connection error:')); //Checks for connection errors
@@ -28,14 +26,45 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/getcard', function(req,res,next){
-  //var url = 'https://api.magicthegathering.io/v1/cards?name=' + req.query.q
-  var url = "https://api.magicthegathering.io/v1/sets/" + req.query.q + "/booster";
+  var url = 'https://api.magicthegathering.io/v1/cards?name=' + req.query.q
+  //var url = "https://api.magicthegathering.io/v1/sets/" + req.query.q + "/booster";
   request(url).pipe(res);
 });
 
-router.post('/addcard', function(req,res,next){
-  var url = 'https://api.magicthegathering.io/v1/cards?id=' + req.query.q
-   
+router.get('/getdeck', function(req, res, next) {
+  Deck.find(function(err, deck){
+    if(err){ return next(err); }
+    res.json(deck);
+  });
+});
+
+router.post('/addcard', function(req, res, next) {
+  var deck = new Deck(req.body);
+  deck.save(function(err, deck){
+    if(err){ return next(err); }
+    res.json(deck);
+  });
+});
+
+router.param('card', function(req, res, next, id) {
+  console.log("In param?");
+  var query = Deck.findById(id);
+  query.exec(function (err, card){
+    if (err) { return next(err); }
+    if (!card) { return next(new Error("can't find comment")); }
+    req.card = card;
+    console.log("Got here!");
+    return next();
+  });
+});
+
+
+router.delete('/removecard/:card', function(req, res) {
+  console.log("Hello!");
+  console.log(req.card);
+  req.card.remove();
+  console.log("Hello again!");
+  res.sendStatus(200);
 });
 
 module.exports = router;
